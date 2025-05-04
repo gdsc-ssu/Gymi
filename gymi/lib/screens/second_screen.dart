5e32df51a325c48f32b0ab52a872163f26877094
jerrymin1221 <jerrymin1221@naver.com>
2025-05-04 21:28:05 +0900

    feat: 모든 스크린 폰트적용 및 exercise3 스크린 연결 (#15)
    * feat: 칼리브레이션 화면 UI 개발
    
    * fix: 칼리브레이션 화면으로 전환 수정
    
    * feat: excercise 1 실행 전 화면 설계
    
    * feat: 설정 화면 설계 (Setting.png 넣기 전)
    
    * fix: exercise 1  실행 전 화면에서 다음화면으로 전환 버튼 vector.png로 수정
    
    * fix: 설정 화면 설계 Setting.png 반영
    
    * fix: 칼리브레이션을 다 한 후 2초후 다음 스크린으로 이동하도록 수정
    
    * fix: mode 설정에서 사용자 카메라 사용 허용하도록 수정
    
    * fix: 불필요한 Import 제거
    
    * feat: 두더지 게임 구현
    
    * chore: gradle 버전 업데이트
    
    * feat: gymi 브금 추가
    
    * fix: 설정에서 소리 on/off 기능 수정
    
    * fix: mode에 따른 스크린 배경색상 적용 (exercise 제외)
    
    * feat: HomeScreen에 필요한 png 파일 추가
    
    * feat: HomeScreen 개발 (폰트 반영 안됨)
    
    * fix: HomeScreen 불필요한 주석 제거
    
    * fix: mode 설정 후 HomeScreen으로  이동
    
    * chore: Google 폰트 추가(Roboto 폰트) 설정
    
    * fix: 모드 선택화면, 홈 화면, 칼리브레이션 화면, 설정 화면 Roboto 폰트 적용
    
    * fix: exercise2 화면 폰트 적용 (Roboto 폰트)
    
    * fix: 홈스크린에서 exercise3 연결
    
    ---------
    
    Co-authored-by: kwonyj1022 <kwonyj1022@naver.com>

--- gymi/lib/screens/second_screen.dart
+++ gymi/lib/screens/second_screen.dart
@@ -1,16 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:eyedid_flutter_example/gaze_overlay.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SecondScreen extends StatefulWidget {
-  final double x;
-  final double y;
-  final Color gazeColor;
-
   const SecondScreen({
     super.key,
-    required this.x,
-    required this.y,
-    required this.gazeColor,
   });
 
   @override
@@ -21,23 +15,23 @@ class _SecondScreenState extends State<SecondScreen> {
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      // ✅ SecondScreen이 생성될 때 Overlay 업데이트
-      GazeOverlay.show(context, widget.x, widget.y, widget.gazeColor);
-    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Second Screen")),
+      appBar: AppBar(
+          title: Text(
+        "Second Screen",
+        style: GoogleFonts.roboto(color: Colors.white),
+      )),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text(
+            Text(
               "이 화면에서도 시선 원이 유지됩니다!",
-              style: TextStyle(fontSize: 20),
+              style: GoogleFonts.roboto(color: Colors.white),
               textAlign: TextAlign.center,
             ),
             const SizedBox(height: 20),
